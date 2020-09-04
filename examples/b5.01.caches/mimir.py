3e3fe78478a48b4821eb66103ab8b703a5045bf2
Dmitry V. Luciv <dluciv@dluciv.name>
2020-09-05 01:02:47 +0300

    mimir

---
+++ examples/b5.01.caches/mimir.py
@@ -0,0 +1,9 @@
+from PyMimircache import Cachecow
+c = Cachecow()
+c.vscsi("trace.vscsi")      # this file is in the data folder on GitHub, other data types also supported
+print(c.stat())
+print(c.get_reuse_distance())
+print(c.get_hit_ratio_dict("LRU", cache_size=20))
+
+c.plotHRCs(["LRU", "LFU", "Optimal"])
+c.heatmap('r', "hit_ratio_start_time_end_time", time_interval=10000000, cache_size=2000)
