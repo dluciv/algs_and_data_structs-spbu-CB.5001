eab8aef618e294bf2320801bd1d88c6f22036916
Dmitry V. Luciv <dluciv@dluciv.name>
2020-09-05 13:13:50 +0300

    cacheout

--- examples/b5/01.caches/cacheout.py
+++ examples/b5/01.caches/cacheout.py
@@ -4,21 +4,21 @@ import timeit
 
 from cacheout import lru_memoize, lifo_memoize, fifo_memoize
 
-@lifo_memoize(maxsize=10)
+@lifo_memoize(maxsize=3)
 def fib_lifo(n):
     if n <= 2:
         return 1
     else:
         return fib_lifo(n-1) + fib_lifo(n-2)
 
-@fifo_memoize(maxsize=10)
+@fifo_memoize(maxsize=3)
 def fib_fifo(n):
     if n <= 2:
         return 1
     else:
         return fib_fifo(n-1) + fib_fifo(n-2)
 
-@lru_memoize(maxsize=10)
+@lru_memoize(maxsize=3)
 def fib_lru(n):
     if n <= 2:
         return 1
