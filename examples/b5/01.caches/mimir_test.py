5ea014656ec733760fdb715602751f933b91f1ac
Dmitry V. Luciv <dluciv@dluciv.name>
2021-09-08 22:56:02 +0300

    Mimir test

--- examples/b5/01.caches/mimir_test.py
+++ examples/b5/01.caches/mimir_test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from PyMimircache import Cachecow
-c = Cachecow()
-c.vscsi("trace.vscsi")      # this file is in the data folder on GitHub, other data types also supported
-print(c.stat())
-print(c.get_reuse_distance())
-print(c.get_hit_ratio_dict("LRU", cache_size=20))
 
-c.plotHRCs(["LRU", "LFU", "Optimal"])
-c.heatmap('r', "hit_ratio_start_time_end_time", time_interval=10000000, cache_size=2000)
+if __name__=='__main__':
+    c = Cachecow()
+    c.vscsi("trace.vscsi")      # this file is in the data folder on GitHub, other data types also supported
+    print(c.stat())
+    print(c.get_reuse_distance())
+    print(c.get_hit_ratio_dict("LRU", cache_size=20))
+
+    c.plotHRCs(["LRU", "LFU", "Optimal"])
+    c.heatmap('r', "hit_ratio_start_time_end_time", time_interval=10000000, cache_size=2000)
