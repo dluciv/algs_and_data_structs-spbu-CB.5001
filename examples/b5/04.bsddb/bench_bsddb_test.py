bf5f65f320061493a2e3974a13330d4e963a763e
Dmitry V. Luciv <dluciv@dluciv.name>
2021-11-18 11:12:59 +0300

    One more surprising test

--- examples/b5/04.bsddb/bench_bsddb_test.py
+++ examples/b5/04.bsddb/bench_bsddb_test.py
@@ -26,7 +26,6 @@ def test_hash(do_print = False):
             print(value.decode('UTF-8'))
         n += 1
 
-
 def test_tree(do_print = False):
     cursor = tree_num_db.cursor()
     cursor.set(rkeys[0])  # ещё бывает set_range — можно отступить назад
@@ -36,11 +35,21 @@ def test_tree(do_print = False):
             print(value.decode('UTF-8'))
         value = cursor.next()[1]
 
+def test_tree_on_hash(do_print = False):
+    n = 0
+    for key in rkeys:
+        value = tree_num_db.get(key)
+        if do_print and n % 100 == 0:
+            print(value.decode('UTF-8'))
+        n += 1
+
 test_hash(True)
 test_tree(True)
+test_tree_on_hash(True)
 
-print(timeit(test_hash, number=1000))
-print(timeit(test_tree, number=1000))
+print("1.", timeit(test_hash, number=1000))
+print("2.", timeit(test_tree, number=1000))
+print("3.", timeit(test_tree_on_hash, number=1000))  # А на каком размере отстанет?
 
 hash_num_db.close()
 tree_num_db.close()
