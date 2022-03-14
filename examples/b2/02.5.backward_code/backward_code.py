99b83dcffe0ed403176d1291822eeb0ea5f3ccfe
Dmitry V. Luciv <dluciv@dluciv.name>
2022-03-14 14:48:43 +0300

    One's completement update

--- examples/b2/02.5.backward_code/backward_code.py
+++ examples/b2/02.5.backward_code/backward_code.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 from __future__ import annotations
-from typing import Union
 from typeguard import typechecked
+from numbers import Integral, Number
 
 from numpy import uint8
-from numbers import Integral
 
 @typechecked
-class OneComp: # (Integral): # Tooo many to implement
+class OneComp(Number): # (Integral): # Tooo many to implement
 
-    def __init__(self, i: Union[Integral, OneComp]):
+    def __init__(self, i: Integral|OneComp):
         if isinstance(i, OneComp):
             self.v: uint8 = i.v
         else:
@@ -19,8 +18,11 @@ class OneComp: # (Integral): # Tooo many to implement
             else:
                 self.v: uint8 = (-i) ^ 0b11111111
 
-    def __eq__(self, o):
-        return self.v == o.v
+    def __eq__(self, o: OneComp) -> bool:
+        return bool(self.v == o.v)
+
+    def __ne__(self, o: OneComp) -> bool:
+        return bool(self.v != o.v)
 
     def __neg__(self) -> OneComp:
         return OneComp(self.v ^ 0b11111111)
@@ -29,7 +31,7 @@ class OneComp: # (Integral): # Tooo many to implement
         return OneComp(self.v + other.v)
 
     def __sub__(self, other: OneComp) -> OneComp:
-        return OneComp(self.v + (-other.v))
+        return OneComp(self.v + (-other.v))  # Number theory here?.. =)
 
     def __str__(self) -> str:
         if self.v & 0b10000000 == 0:
@@ -38,4 +40,4 @@ class OneComp: # (Integral): # Tooo many to implement
             return '-' + str(-self)
     
     def __repr__(self) -> str:
-        return bin(self.v)
+        return bin(self.v) + ' (=' + str(self) + ')'
--- examples/b2/02.5.backward_code/backward_code_test.py
+++ examples/b2/02.5.backward_code/backward_code_test.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-from __future__ import annotations
 import unittest
 
 from backward_code import OneComp
@@ -15,9 +13,12 @@ class TestArithmetic(unittest.TestCase):
         self.assertNotEqual(self.v0, -self.v0)
         self.assertEqual(self.v0, -(-self.v0))
 
-    def test_sum(self):
+    def test_add(self):
         self.assertEqual(self.v1 + self.v2, OneComp(-2))
-        self.assertEqual(self.v1 + self.v2, OneComp(-2))
+        self.assertEqual(self.v1 + -self.v2, OneComp(22))
+        self.assertEqual(-self.v1 + self.v2, OneComp(-22))
+
+    def test_sub(self):
         self.assertEqual(self.v1 - self.v2, OneComp(22))
         self.assertEqual(self.v2 - self.v1, OneComp(-22))
 
@@ -26,6 +27,9 @@ class TestArithmetic(unittest.TestCase):
 
     def test_fail(self):
         pass  # self.fail()
+
+    def test_str(self):
+        self.assertEqual(str(self.v2), "-12")
 
 if __name__ == '__main__':
     unittest.main()
